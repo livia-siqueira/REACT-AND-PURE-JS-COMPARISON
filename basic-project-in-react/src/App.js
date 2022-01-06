db636ad1e3e2d110d10916af4c066585ce088ca8
Lívia Siqueira <livia.siqueira12@outlook.com>
2022-01-05 23:47:21 -0300

    feat: fineshed

--- basic-project-in-react/src/App.js
+++ basic-project-in-react/src/App.js
@@ -3,7 +3,11 @@ import { ItemList } from "./components/ItemList";
 
 function App() {
   return (
-   <ItemList title="Learn React"/>
+    <>
+      <ItemList title="Learn React" />
+      <ItemList title="Task history" />
+      <ItemList title="Task math" />
+    </>
   );
 }
 
